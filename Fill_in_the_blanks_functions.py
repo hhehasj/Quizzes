b45b4b1ef32708a53ebe12d7f660c2f0facac307
hhehasj <alfredlennardaquino@gmail.com>
2024-03-18 20:25:15 +0700

    Able to display a statement with the missing word in Fill_in_the_blanks_functions.py

--- Fill_in_the_blanks_functions.py
+++ Fill_in_the_blanks_functions.py
@@ -1,30 +1,27 @@
-<<<<<<< HEAD
 replacement_words = []
+lists_of_statements = []
 
 
 def main():
     users_statement = input("Enter your statement:\n")
-    users_statement_as_list = users_statement.split()
+    users_statements_as_list = users_statement.split()
 
     word_to_replace = input("What word do you want to replace:\n")
 
     #   replacing chosen word with underscores
-    for words in users_statement_as_list:
+    for words in users_statements_as_list:
         if words == word_to_replace:
             underscored_word = len(words) * "_"
             replacement_words.append(underscored_word)
-            index_of_underscored_word = users_statement_as_list.index(words)
-            users_statement_as_list.pop(index_of_underscored_word)
-            users_statement_as_list.insert(index_of_underscored_word, underscored_word)
 
+            index_of_underscored_word = users_statements_as_list.index(words)
+            users_statements_as_list.pop(index_of_underscored_word)
+            users_statements_as_list.insert(index_of_underscored_word, underscored_word)
+            lists_of_statements.append(users_statements_as_list)
 
-
-
-#   putting replace_words into the statement
+    for list_of_statements in lists_of_statements:
+        for words in list_of_statements:
+            print(words, end=" ")
 
 if __name__ == "__main__":
     main()
-=======
-if __name__ == "__main__":
-    command_line()
->>>>>>> a6cbd2fd5ab98b6eaad7900ed2c4120b0dfa3c99
