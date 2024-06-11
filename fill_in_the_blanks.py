008c6196b3df30d91c33c6028f3cccba20262be0
hhehasj <alfredlennardaquino@gmail.com>
2024-06-11 09:11:52 +0700

    Decided to do the project all over again. This time with a different approach. (I hope)

---
+++ fill_in_the_blanks.py
@@ -0,0 +1,11 @@
+statements = ("Gregor Mendel is the father of _________", 
+              "ROG stands for Republic of ______", 
+              "Steve Jobs is known for _____")
+answers = ("genetics", 
+           "Gamers", 
+           "Apple")
+
+start: str = input("WELCOME!!!\nStatements and choices will show up.\nChoose which fits best in the blanks.\nReady? (Y/N):")
+
+if start.upper == "Y":
+    start
