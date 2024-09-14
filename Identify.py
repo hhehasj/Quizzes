c71d0bfd8d888fb5d48d039e3976de869377ed4d
hhehasj <alfredlennardaquino@gmail.com>
2024-09-14 21:01:39 +0700

    Got the commands sort of working. Cannot start a quiz yet.

--- Identify.py
+++ Identify.py
@@ -1,41 +1,27 @@
-questions = ("In what country did the first Starbucks open outside of North America?",
-             "In a website browser address bar, what does “www” stand for?",
-             "Who was the first woman pilot to fly solo across the Atlantic?",
-             "Who was the first woman to win a Nobel Prize?",
-             "Chrome, Safari, Firefox and Explorer are different types of what?")
-
-answers = ("Japan", "World Wide Web", "Amelia Earhart", "Marie Curie", "Web Browsers")
+questions = []
+answers = []
 guesses = []
 score = 0
 question_num = 0
+not_started = True
 
-for question in questions:
-    print(question)
-    guess = input("Your guess: ")
-    guesses.append(guess)
-    if guess == answers[question_num]:
-        print("Correct!")
-        score += 1
-        print("――――――――――――――――――――――――――――")
+# Commands
+choices = input("Questions (que)\nAnswers (ans)\nStart quiz (start)\nexit (ex)\n>")
+
+while not_started:
+    if choices == "que" or choices == "QUE":
+        que_of_user = input("――――――――――――――――――――――――――――\n>>")
+        questions.append(que_of_user)
+
+    elif choices == "ans" or choices == "ANS":
+        ans_of_user = input("――――――――――――――――――――――――――――\n>>>")
+        answers.append(ans_of_user)
+
+    elif choices == "start" or choices == "START":
+        not_started = False
+
+    elif choices == "ex" or choices == "EX":
+        quit()
+
     else:
-        print("Wrong!")
-        print(f"Correct answer: {answers[question_num]}")
-        print("――――――――――――――――――――――――――――")
-
-    question_num += 1
-
-print("                   Results                   ")
-print("――――――――――――――――――――――――――――")
-
-print("Answers: ", end="|")
-for answer in answers:
-    print(answer, end="|")
-print()
-
-print("Guesses: ", end="|")
-for guess in guesses:
-    print(guess, end="|")
-print()
-
-score = int(score / len(questions) * 100)
-print(f"Your score is: {score}%")
+        print(f"\"{choices}\" is not a valid command.\n――――――――――――――――――――――――――――")
