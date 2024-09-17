0a74989013f264c6f42dc412d84bc44bacca71f5
hhehasj <alfredlennardaquino@gmail.com>
2024-09-17 19:40:28 +0700

    The commands work, but the actual quiz is not done yet

--- Identify.py
+++ Identify.py
@@ -3,25 +3,40 @@ answers = []
 guesses = []
 score = 0
 question_num = 0
-not_started = True
 
-# Commands
-choices = input("Questions (que)\nAnswers (ans)\nStart quiz (start)\nexit (ex)\n>")
+while True:
+    print("―" * 20)
+    choices = input("Make questions (que)\n"
+                    "Answers to the questions (ans)\n"
+                    "Start quiz (start)\n"
+                    "Exit (ex)\n> ")
+    print("―" * 20)
 
-while not_started:
-    if choices == "que" or choices == "QUE":
-        que_of_user = input("――――――――――――――――――――――――――――\n>>")
-        questions.append(que_of_user)
+    if choices == "que" or choices == "Que" or choices == "QUE":
+        while True:
+            users_question = input("Question: ")
 
-    elif choices == "ans" or choices == "ANS":
-        ans_of_user = input("――――――――――――――――――――――――――――\n>>>")
-        answers.append(ans_of_user)
+            if users_question == "b" or users_question == "B":
+                break
+            else:
+                questions.append(users_question)
 
-    elif choices == "start" or choices == "START":
-        not_started = False
+    elif choices == "ans" or choices == "Ans" or choices == "ANS":
+        while True:
+            users_answer = input("Answer to the question: ")
 
-    elif choices == "ex" or choices == "EX":
-        quit()
+            if users_answer == "b" or users_answer == "B":
+                print("\n")
+                break
+            else:
+                answers.append(users_answer)
+
+    elif choices == "start" or choices == "Start" or choices == "START":
+        pass
+
+    elif choices == "ex" or choices == "Ex" or choices == "EX":
+        break
 
     else:
-        print(f"\"{choices}\" is not a valid command.\n――――――――――――――――――――――――――――")
+        print(f"\033[91m \"{choices}\" is not a valid command.\033[0m")
+
