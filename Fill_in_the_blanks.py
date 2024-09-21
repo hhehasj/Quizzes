718acb6361a4689c66bc9453572ab7861c3eff65
hhehasj <alfredlennardaquino@gmail.com>
2024-09-21 21:15:39 +0700

    Finished the commands. (start) does not work yet, though

--- Fill_in_the_blanks.py
+++ Fill_in_the_blanks.py
@@ -1,41 +1,41 @@
-questions = ("The film, \"The ____ Redemption\" is based on a Stephen King novella.",
-             "\"Jurassic ____\" is a blockbuster film directed by Steven Spielberg.",
-             "In \"The Dark ____,\" Bruce Wayne is the alter ego of the superhero Batman.",
-             "The character Jack Dawson appears in the film \"____,\" directed by James Cameron.",
-             "The animated film, \"Toy ____\" features a cowboy doll named Woody.")
+statements: list[str] = []
+blanks: list[str] = []
+guesses: list[str] = []
+score: int = 0
+question_num: int = 0
+not_ended = True
 
-answers = ("Shawshank", "Park", "Knight", "Titanic", "Story")
-guesses = []
-score = 0
-question_num = 0
+while not_ended:
+    print("―" * 20)
+    choices: str = input("Make questions (make)\n"
+                         "Start quiz (start)\n"
+                         "Exit (ex)\n"
+                         "> ")
+    print("―" * 20)
 
-for question in questions:
-    print(question)
-    guess = input("Your guess: ")
-    guesses.append(guess)
-    if guess == answers[question_num]:
-        score += 1
-        print("Correct!")
-        print("――――――――――――――――――――――――――――")
+    if choices == "make" or choices == "Make" or choices == "MAKE":
+        while True:
+            users_statement: str = input("Statement: ")
+
+            if users_statement == "b" or users_statement == "B":
+                break
+
+            else:
+                statements.append(users_statement)
+
+            chosen_word: str = input("Chosen word: ")
+            blank: str = "_" * len(chosen_word)
+
+            if chosen_word == "b" or chosen_word == "B":
+                break
+            else:
+                blanks.append(blank)
+
+    elif choices == "start" or choices == "Start" or choices == "START":
+        ...
+
+    elif choices == "ex" or choices == "Ex" or choices == "EX":
+        break
+
     else:
-        print("Wrong!")
-        print(f"Correct answer: {answers[question_num]}")
-        print("――――――――――――――――――――――――――――")
-    question_num += 1
-
-
-print("                  Results                     ")
-print("――――――――――――――――――――――――――――")
-
-print("Answers: ", end="|")
-for answer in answers:
-    print(answer, end="|")
-print()
-
-print("Guesses: ", end="|")
-for guess in guesses:
-    print(guess, end="|")
-print()
-
-score = int(score / len(questions) * 100)
-print(f"Your score is: {score}%")
+        print(f"\033[91m{choices} is not a valid command\033[0m")
