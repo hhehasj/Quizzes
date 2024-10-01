386f9ec23263dfab01fe131a61ea61989d6b8dbb
hhehasj <alfredlennardaquino@gmail.com>
2024-10-01 16:32:35 +0700

    Added a README.md. Cannot start the quiz, but the commands theoretically work.

--- Multiple_choice.py
+++ Multiple_choice.py
@@ -1,37 +1,51 @@
 # Quiz
+questions: list[str] = []
+answers: list[str] = []
+options_list: list[str] = []
+guesses: list[str] = []
+score: int = 0
+question_num: int = 0
+not_ended: bool = True
+break_loop: bool = False
+letters = ("A. ", "B. ", "C. ", "D. ")
 
-questions = ("How many elements are in the periodic table?: ",
-             "Which animal lays the largest eggs?: ",
-             "What is the most abundant gas in Earth's atmosphere?: ",
-             "How many bones are in the human body?: ",
-             "Which planet in the solary system is the hottest?: ")
 
-options = (("A. 116", "B. 117", "C. 118", "D. 119"),
-           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
-           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
-           ("A. 206", "B. 207", "C. 208", "D. 210"),
-           ("A. Mercury", "B. Venus", "C. Earth", "D Mars"))
+while not_ended:
+    print("―" * 20)
+    choices: str = input("Make questions (make)\n"
+                         "Start quiz (start)\n"
+                         "Exit (ex)\n"
+                         "> ")
+    print("―" * 20)
 
-answers = ("C", "D", "A", "A", "B")
-guesses = []
-score = 0
-question_num = 0
+    if choices == "make" or choices == "Make" or choices == "MAKE":
+        while True:
+            users_question: str = input("Question: ")
+            if users_question == "b" or users_question == "B":
+                break
+            else:
+                questions.append(users_question)
 
-for question in questions:
-    print("-----------------------------")
-    print(question)
-    for option in options[question_num]:
-        print(option)
-    guess = input("Enter (A, B, C, D): ").upper()
-    guesses.append(guess)
-    if guess == answers[question_num]:
-        score += 1
-        print("CORRECT!")
+            users_answer: str = input("Answer: ")
+            if users_answer == "b" or users_answer == "B":
+                break
+            else:
+                answers.append(users_answer)
+
+            for number_of_letters in range(len(letters)):
+                users_options: str = input("Options: ")
+                if users_options == "b" or users_options == "B":
+                    break
+                else:
+                    options_list.append(users_options)
+                print(options_list)
+
+    elif choices == "start" or choices == "Start" or choices == "START":
+        ...
+    elif choices == "ex" or choices == "Ex" or choices == "EX":
+        break
     else:
-        print("WRONG!")
-        print(f"{answers[question_num]} is the correct answer.")
-
-    question_num += 1
+        print(f"\033[91m\"{choices}\" is not a valid command\033[0m")
 
 
 print("-----------------------------")
@@ -50,3 +64,5 @@ print()
 
 score = int(score / len(questions) * 100)
 print(f"Your score is: {score}%")
+
+print(questions, answers, options_list)
---
+++ README.md
@@ -0,0 +1,11 @@
+# Quizzes
+
+### Consists of:
+* Multiple choices 
+* Fill in the blanks
+* Identifying
+
+What I have done is that I've made the simple versions first. 
+Meaning, I put the questions and answers. Then the second iteration 
+would allow user's inputs, so you can enter your own questions and answers.
+Finally, making them into GUI's.
