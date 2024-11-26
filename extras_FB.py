4b648788194b07f4ec734d85648841f9d764aa9d
hhehasj <alfredlennardaquino@gmail.com>
2024-11-26 13:49:14 +0700

    The textboxes are functional. It is doing what it should, but little optimizations will be done soon.

--- extras_FB.py
+++ extras_FB.py
@@ -1,9 +1,10 @@
 import customtkinter as ctk
 from PIL import Image
+from tkinter.messagebox import showerror
 
 # Global Variables
-Users_Questions: list[str] = []
-Users_Answers: list[str] = []
+Users_Statements: list[str] = []
+Users_Blanks: list[str] = []
 Users_Guess: list[str] = []
 score: int = 0
 start_button_pressed: bool = False
@@ -18,18 +19,71 @@ def quiz_started(value: bool):
 def get_start_button_pressed():
     return start_button_pressed
 
+
 class Make_Statements(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(master=parent)
 
         def stores_and_displays():
-            ...
+            """
+            Displays the output of the statements that will be used in the quiz.
+            Then the other textboxes will be empty.
+            """""
 
-        def store_question() -> list[str]:
-            ...
+            if self.statements_textbox.get(1.0, "end") == "\n" and self.chosen_word_textbox.get(1.0, "end") == "\n":
+                showerror(title="Empty field", message="Both textboxes are empty.")
 
-        def store_answer() -> list[str]:
-            ...
+            elif self.statements_textbox.get(1.0, "end") == "\n" or self.chosen_word_textbox.get(1.0, "end") == "\n":
+                showerror(title="Empty field", message="One of the textboxes is empty.")
+
+            else:
+                users_statements: list[str] = store_statements()
+                statement_with_blank: list[str] = statement_to_blank()
+
+                for number, statement in enumerate(users_statements):
+                    self.preview_box.configure(state="normal")
+
+                    self.preview_box.insert("end", f"{statement}\n")
+                    Users_Statements.append(statement)
+
+                    self.preview_box.insert("end", f"{statement_with_blank[number]}\n")
+                    Users_Blanks.append(statement_with_blank[number])
+
+                    self.preview_box.insert("end", "―" * 15 + "\n")
+
+                self.preview_box.configure(state="disabled")
+
+                self.statements_textbox.delete(0.0, "end")
+                self.chosen_word_textbox.delete(0.0, "end")
+
+        def store_statements() -> list[str]:
+            statements: list[str] = []
+            users_input: str = self.statements_textbox.get("1.0", "end")
+
+            users_input = users_input.replace("\t", "")
+            users_input: list[str] = users_input.splitlines()
+
+            for statement in users_input:
+                statements.append(statement)
+            return statements
+
+        def statement_to_blank() -> list[str]:
+            new_statement: list[str] = []
+            initial_statement: str = self.statements_textbox.get(1.0, "end")
+            chosen_word: str = self.chosen_word_textbox.get(1.0, "end")
+
+            initial_statement = initial_statement.replace("\t", "")
+            statements_list: list[str] = initial_statement.splitlines()
+            chosen_word = chosen_word.replace("\t", "")
+            chosen_words_list: list[str] = chosen_word.splitlines()
+
+            for index, statement in enumerate(statements_list):
+                if statement.find(chosen_words_list[index]) != -1:
+                    new_statement.append(statement.replace(chosen_words_list[index], "_" * len(chosen_words_list[index])))
+                else:
+                    showerror(title="Word not found", message="The word you chose is not in the statement.")
+
+            return new_statement
 
         # Frame's configuration
         self.configure(fg_color=("#ebebeb", "#808080"))
@@ -37,15 +91,15 @@ class Make_Statements(ctk.CTkFrame):
 
         # WIDGETS
         # Where you enter your questions
-        self.question_textbox_label = ctk.CTkLabel(
+        self.statements_textbox_label = ctk.CTkLabel(
             self,
-            text="Questions",
+            text="Statements",
             font=("Arial", 25),
             text_color=("black", "white"),
         )
-        self.question_textbox_label.place(relx=0.25, rely=0.15, anchor="center")
+        self.statements_textbox_label.place(relx=0.25, rely=0.15, anchor="center")
 
-        self.question_textbox = ctk.CTkTextbox(
+        self.statements_textbox = ctk.CTkTextbox(
             self,
             font=("Helvetica", 15),
             fg_color="white",
@@ -54,18 +108,18 @@ class Make_Statements(ctk.CTkFrame):
             wrap="word",
             text_color="black"
         )
-        self.question_textbox.place(relx=0.26, rely=0.3, anchor="center", relwidth=0.35, relheight=0.2)
+        self.statements_textbox.place(relx=0.26, rely=0.3, anchor="center", relwidth=0.35, relheight=0.2)
 
         # Where you enter the answers to the questions
-        self.answer_textbox_label = ctk.CTkLabel(
+        self.chosen_word_to_blank_label = ctk.CTkLabel(
             self,
-            text="Answers",
-            font=("Arial", 25),
+            text="The word you want\nto turn into a blank",
+            font=("Arial", 20),
             text_color=("black", "white"),
         )
-        self.answer_textbox_label.place(relx=0.25, rely=0.5, anchor="center")
+        self.chosen_word_to_blank_label.place(relx=0.25, rely=0.48, anchor="center")
 
-        self.answer_textbox = ctk.CTkTextbox(
+        self.chosen_word_textbox = ctk.CTkTextbox(
             self,
             font=("Helvetica", 15),
             fg_color="white",
@@ -74,7 +128,7 @@ class Make_Statements(ctk.CTkFrame):
             wrap="word",
             text_color="black"
         )
-        self.answer_textbox.place(relx=0.26, rely=0.65, anchor="center", relwidth=0.35, relheight=0.2)
+        self.chosen_word_textbox.place(relx=0.26, rely=0.65, anchor="center", relwidth=0.35, relheight=0.2)
 
         # stores the question and answer
         self.next_button = ctk.CTkButton(
@@ -86,7 +140,7 @@ class Make_Statements(ctk.CTkFrame):
             border_color="black",
             border_width=3,
             border_spacing=10,
-            # command=stores_and_displays
+            command=stores_and_displays
         )
         self.next_button.place(relx=0.25, rely=0.88, anchor="center")
 
